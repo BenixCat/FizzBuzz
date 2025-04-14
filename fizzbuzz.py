54cd6276630763b36ec52a15a5675371ad4936fa
BenixCat <141242449+BenixCat@users.noreply.github.com>
2025-04-14 16:48:42 +0200

    Add comments

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,14 @@
+# FizzBuzz
 for fzbz in range(1, 101,1):
+# If the number is divisible by 3 and 5, print FizzBuzz
     if fzbz % 3 == 0 and fzbz % 5 == 0:
         print("FizzBuzz")
+    # If the number is divisible by 5, print Fizz
     elif fzbz % 5 == 0:
         print("Buzz")
+    # If the number is divisible by 3, print Buzz
     elif fzbz % 3 == 0:
         print("Fizz")
+    # If the number is not divisible by 3 or 5, print the number
     else:
         print(fzbz)
